26df4f85110acc814bd9a9e87d1a61e9d4ea27a5
ana cunha <ana.cunha@zup.com.br>
2026-04-22 11:27:59 -0300

    feat(usuario): adiciona tratamento global de exceções para email duplicado e senha inválida

---
+++ src/main/java/com/empresa/smartestrelas/usuario/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.empresa.smartestrelas.usuario.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(EmailJaCadastradoException.class)
+    public ResponseEntity<String> handleEmailJaCadastrado(EmailJaCadastradoException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+        //Retorna o erro 409 (Conflict) quando você tenta criar algo que já existe
+    }
+
+    @ExceptionHandler(SenhaInvalidaException.class)
+    public ResponseEntity<String> handleSenhaInvalida(SenhaInvalidaException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+        //Retorna erro 400 (Bad Request) quando tenta inserir uma senha inválida
+    }
+}
+
