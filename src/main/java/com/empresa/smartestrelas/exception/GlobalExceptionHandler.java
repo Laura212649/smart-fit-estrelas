c9813c18ea18ac7939869da621a74fa95d41082e
ana cunha <ana.cunha@zup.com.br>
2026-04-27 10:55:10 -0300

    refactor(exception): mover GlobalExceptionHandler para raiz do projeto

--- src/main/java/com/empresa/smartestrelas/exception/GlobalExceptionHandler.java
+++ src/main/java/com/empresa/smartestrelas/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.empresa.smartestrelas.usuario.exception;
+package com.empresa.smartestrelas.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
