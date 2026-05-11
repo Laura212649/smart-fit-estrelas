d2000e623c745b2cd9d35c4e44b14d064c6f58b9
evellyn severo <evellyn.severo@zup.com.br>
2026-05-11 10:09:58 -0300

    refactor: Atualiza o nome da classe

---
+++ src/main/java/com/empresa/smartestrelas/repository/EquipamentoRepository.java
@@ -0,0 +1,7 @@
+package com.empresa.smartestrelas.repository;
+
+import com.empresa.smartestrelas.model.Equipamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipamentoRepository extends JpaRepository<Equipamento, Long> {
+}
--- src/main/java/com/empresa/smartestrelas/repository/EquipmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.empresa.smartestrelas.repository;
-
-import com.empresa.smartestrelas.model.Equipament;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EquipmentRepository extends JpaRepository<Equipament, Long> {
-}
