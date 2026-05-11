288f37df3883424875aa22d92b96225d12f821c5
evellyn severo <evellyn.severo@zup.com.br>
2026-05-11 09:56:21 -0300

    refactor: Atualização no ExercicioRepository para filtragem por categoria, equipamento e músculo,adicionando métodos de busca customizados na interface.

--- src/main/java/com/empresa/smartestrelas/repository/ExercicioRepository.java
+++ src/main/java/com/empresa/smartestrelas/repository/ExercicioRepository.java
@@ -4,6 +4,17 @@ import com.empresa.smartestrelas.model.Exercicio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ExercicioRepository extends JpaRepository<Exercicio, Long> {
+    // Filtra exercícios por categoria (ex: Chest, Legs) [cite: 77]
+    List<Exercicio> findByCategory(String category);
+
+    // Filtra exercícios que contenham um equipamento específico [cite: 77]
+    List<Exercicio> findByEquipment_Id(Long equipmentId);
+
+    // Filtra exercícios que tenham um músculo primário específico [cite: 77]
+    List<Exercicio> findByPrimaryMuscles_Id(Long muscleId);
 }
+
