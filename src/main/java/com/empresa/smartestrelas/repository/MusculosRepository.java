8f35775ed21df4f98547a69d17e94d6deeec2df4
evellyn severo <evellyn.severo@zup.com.br>
2026-05-12 11:05:38 -0300

    refactor: ajusta MusculosRepository para refletir mudanças na classe Model

--- src/main/java/com/empresa/smartestrelas/repository/MusculosRepository.java
+++ src/main/java/com/empresa/smartestrelas/repository/MusculosRepository.java
@@ -2,6 +2,8 @@ package com.empresa.smartestrelas.repository;
 
 import com.empresa.smartestrelas.model.Musculos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MusculosRepository  extends JpaRepository<Musculos, Long> {
 }
