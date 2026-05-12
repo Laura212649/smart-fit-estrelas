888ed0786ae6e75b4bfc804f92ca792168f4859e
evellyn severo <evellyn.severo@zup.com.br>
2026-05-12 11:07:02 -0300

    fix: corrige repositório de equipamentos para suportar carga inicial de dados

--- src/main/java/com/empresa/smartestrelas/repository/EquipamentoRepository.java
+++ src/main/java/com/empresa/smartestrelas/repository/EquipamentoRepository.java
@@ -2,6 +2,8 @@ package com.empresa.smartestrelas.repository;
 
 import com.empresa.smartestrelas.model.Equipamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EquipamentoRepository extends JpaRepository<Equipamento, Long> {
 }
