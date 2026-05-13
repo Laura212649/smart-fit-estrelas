aaaa76488b08079d19dd1445284b6b56a09ea432
evellyn severo <evellyn.severo@zup.com.br>
2026-05-13 03:46:07 -0300

    feat: adiciona enums Goal e SplitFocus para regras do plano de treino

---
+++ src/main/java/com/empresa/smartestrelas/model/Goal.java
@@ -0,0 +1,7 @@
+package com.empresa.smartestrelas.model;
+
+public enum Goal {
+    WEIGHT_LOSS,
+    MUSCLE_GAIN,
+    CONDITIONING
+}
---
+++ src/main/java/com/empresa/smartestrelas/model/SplitFocus.java
@@ -0,0 +1,5 @@
+package com.empresa.smartestrelas.model;
+
+public enum SplitFocus {
+    UPPER_PUSH, UPPER_PULL, LOWER_PUSH, LOWER_PULL, FULL_BODY
+}
